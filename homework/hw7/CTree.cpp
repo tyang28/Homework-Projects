ce4a43e684ab17a7041f629d0787861ef3c10b5a
Teng-Ju Yang <tyang28@jhu.edu>
2018-11-17 22:55:56 -0500

    fixed everything, helper functions were overcomplicating things

--- homework/hw7/CTree.cpp
+++ homework/hw7/CTree.cpp
@@ -14,165 +14,87 @@ using std::string;
 using std::endl;
 using std::cout;
 
-/*
-CTree::CTree(char tch) : data(tch), kids(NULL), sibs(NULL), prev(NULL) {
-  
-}
-*/
 
-CTree::CTree(char tch) {
-  data = tch;
-  kids = nullptr; 
-  sibs = nullptr;
-  prev = nullptr;
+CTree::CTree(char tch) : data(tch), kids(nullptr), sibs(nullptr), prev(nullptr) {
 }
 
+bool CTree::addChild(CTree *root) {
+  bool check = false;
+  if(kids == nullptr) {
+    kids = root;
+    kids->prev = this;
+    check = true;
+    return check;
+  }
+  else {
+    return kids->addSibling(root);
+  }
+}
 
-bool CTree::findVal(char ch) {
-  
+
+bool CTree::addSibling(CTree *root) {
+
   bool check = false;
   
-  CTree *cur = sibs;
-  if(data == ch) {
-      check = true;
-      return check;
+  if((root->prev != nullptr) || (root->sibs != nullptr)) {
+    delete root;
+    return check;
   }
-  else if(kids != nullptr) {
-    if(kids->data == ch) {
+  if(prev == nullptr && sibs == nullptr) {
+    delete root;
+    return false;
+  }
+  CTree *cur = this;
+  if(sibs == nullptr) {
+    
+    if(data == root->data) {
+      delete root;
+      return check;
+    }
+    sibs = root;
+    sibs->prev = this;
+  }
+  else {
+    if(root->data < cur->data) {
+      root->sibs = this;
+      root->prev = prev;
+      prev->kids = root;
+      prev = root;
       check = true;
       return check;
     }
-  }
-  else {
-    while(cur != nullptr) {
-      if(cur->data == ch) {
+    while(cur->sibs != nullptr) {
+      if(cur->data == root->data) {
+	delete root;
+	return check;
+      }
+      if(root->data < cur->data) {
+	root->sibs = cur;
+	root->prev = cur->prev;
+	cur->prev->sibs = root;
+	cur->prev = root;
 	check = true;
 	return check;
       }
       cur = cur->sibs;
     }
-  }
-
-  
-  return check;
-}
-
-
-bool CTree::addChild(CTree *root) {
-  bool check = false;
-  
-  if(checkRoot(root)) {  
-    CTree *nc = root;
-    CTree *cur = sibs;
-    if(kids == nullptr) {
-      kids = nc;
-      check = true;
-      printf("11\n");
+    if(cur->data == root->data) {
+      delete root;
       return check;
     }
-    else if(kids != nullptr) {  
-      if(nc->data < kids->data) {
-        nc->sibs = kids;
-        kids->prev = nc;
-        check = true;
-        return check;
-      }
-      else if(kids->data < nc->data) {
-	printf("3\n");
-        while(cur != nullptr) {
-          if(nc->data < cur->data) {
-            nc->sibs = cur;
-            nc->prev = cur->prev;
-	    cur->prev->sibs = nc;
-            cur->prev = nc;
-            check = true;
-            return check;
-          }
-          cur = cur->sibs;
-        }
-        if(cur == nullptr){
-	  printf("4\n");
-          nc->sibs = nullptr;
-          nc->prev = cur;
-          cur->sibs = nc;
-          check = true;
-          return check;
-        }
-      }
+    if(root->data < cur->data) {
+      root->sibs = cur;
+      root->prev = cur->prev;
+      cur->prev->sibs = root;
+      cur->prev = root;
+      check = true;
+      return check;
     }
-    
+    cur->sibs = root;
+    cur->sibs->prev = cur;
   }
-    
+  check = true;
   return check;
-}
-
-bool CTree::checkRoot(CTree *root) {
-  bool check = true;
-  if(root == nullptr) {
-    cout << "1" << endl;
-    check = false;
-    return check;
-  }
-  else if((root->prev != nullptr) || (root->sibs != nullptr)) {
-    cout << "2" << endl;
-    check = false;
-    return check;
-  }
-  else if(findVal(root->data)) {
-    cout << "3" << endl;
-    check = false;
-    return check;
-  }
-
-  return check;
-}
-
-bool CTree::addSibling(CTree *root) {
-  
-  bool check = false;
-  /*
-  if(findVal(root->data)) {
-    return check;
-  }
-  else {
-    CTree *nc = root;
-    CTree *cur = sibs;
-    if(cur == nullptr) {
-      cur = nc;
-    }
-    else if(cur != nullptr) {
-      if(nc->data < cur->data) {
-        nc->sibs = cur;
-        nc->prev = kids;
-        cur->prev = nc;
-        check = true;
-        return check;
-      }
-      else if(cur->data < nc->data) {
-        while(cur->sibs != nullptr) {
-          if(nc->data < cur->sibs->data) {
-            nc->sibs = cur->sibs;
-            nc->prev = cur;
-            cur->sibs->prev = nc;
-            cur->sibs = nc;
-            check = true;
-            return check;
-          }
-          cur = cur->sibs;
-        }
-        if(cur->sibs == nullptr){
-          nc->sibs = nullptr;
-          nc->prev = cur;
-          cur->sibs = nc;
-          check = true;
-          return check;
-        }
-      }
-    }
-  }
-  */
-  return check;
-  
 }
 
 string CTree::toString() {
@@ -188,73 +110,44 @@ string CTree::toString() {
 }
 
 bool CTree::addChild(char ch) {
-  bool check = false;
-  
-  if(findVal(ch)) {
-    return check;
+  if(kids == nullptr) {
+    CTree *nc = new CTree(ch);
+    kids = nc;
+    nc->prev = this;
+    return true;
   }
-  else if(!findVal(ch)) {
-    CTree nc(ch);
-    addChild(&nc);
-    check = true;
-
-
-    /*
-    if(addChild(&nc)) {
-      check = true;
-      return check;
-    }
-    */
-    
-    /*
-    CTree *cur = this->kids;
-    if(cur == nullptr) {
-      cur = &nc;
-    }
-    else if(cur != nullptr) {
-      if(nc.data < cur->data) {
-	nc.sibs = cur;
-	nc.prev = nullptr;
-	cur->prev = &nc;
-	check = true;
-	return check;
-      }
-      else if(cur->data < nc.data) {
-	while(cur->sibs != nullptr) {
-	  if(nc.data < cur->sibs->data) {
-	    nc.sibs = cur->sibs;
-	    nc.prev = cur;
-	    cur->sibs->prev = &nc;
-	    cur->sibs = &nc;
-	    check = true;
-	    return check;
-	  } 
-	  cur = cur->sibs;
-	}
-	if(cur->sibs == nullptr){
-	  nc.sibs = nullptr;
-	  nc.prev = cur;
-	  cur->sibs = &nc;
-	  check = true;
-	  return check;
-	}	
-      }
-      
-    }
-    */
+  else {
+    return kids->addSibling(ch);
   }
-
-  return check;
 }
+
+
+bool CTree::addSibling(char ch) {
+  CTree *nc = new CTree(ch);
+  
+  return addSibling(nc);
+}
+
 
 CTree& CTree::operator^(CTree& rt) {
   addChild(&rt);  
-  return rt;
+
+  return *this;
 }
+
 
 CTree::~CTree() {
+  if(kids) {
+    delete kids;
+  }
+  if(sibs) {
+    delete sibs;
+  }
 }
 
-
+std::ostream& operator<<(std::ostream& os, CTree& rt) {
+  os << rt.toString();
+  return os;
+}
 
 
